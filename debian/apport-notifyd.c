eb2d88cdea8b708968bf554651a1e711a1fc8b4d
Yuru Shao <shaoyuru@gmail.com>
2015-03-22 22:41:28 -0400

    adjust indent

--- debian/apport-notifyd.c
+++ debian/apport-notifyd.c
@@ -1,13 +1,13 @@
 /*
-***************************************************************************
-*                  Copyright (C) Ritesh Raj Sarraf <rrs@debian.org>       *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published   	  *
-* by the Free Software Foundation, either version 3 or (at your option)   *
-* any later version. This program is distributed without any warranty.    *
-***************************************************************************
-*/
+ ***************************************************************************
+ *                  Copyright (C) Ritesh Raj Sarraf <rrs@debian.org>       *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU General Public License as published   	  *
+ * by the Free Software Foundation, either version 3 or (at your option)   *
+ * any later version. This program is distributed without any warranty.    *
+ ***************************************************************************
+ */
 
 #include <sys/inotify.h>
 #include <syslog.h>
@@ -23,21 +23,21 @@
 
 int checkFileExtn(const char* extn, const char* CRASH_EXTN)
 {
-	#define CRASH_EXTN ".crash"
-	size_t extnLength;
-	size_t apportLength;
+#define CRASH_EXTN ".crash"
+    size_t extnLength;
+    size_t apportLength;
 
-	extnLength = strlen(extn);
-	apportLength = strlen(CRASH_EXTN);
-	// syslog(LOG_NOTICE,"extnLength is %d and apportLength is %d\n", extnLength, apportLength);
+    extnLength = strlen(extn);
+    apportLength = strlen(CRASH_EXTN);
+    // syslog(LOG_NOTICE,"extnLength is %d and apportLength is %d\n", extnLength, apportLength);
 
     // if a file's name is shorter than ".crash",
     // apparently it's not a crash dump 
-	if(apportLength > extnLength) return 0;
+    if(apportLength > extnLength) return 0;
 
-	/* Check if it is a .crash file */
-	// syslog(LOG_NOTICE,"strcmp length is %s\n", &extn[extnLength - apportLength]);
-	return (strcmp(&extn[extnLength - apportLength], CRASH_EXTN)) == 0;
+    /* Check if it is a .crash file */
+    // syslog(LOG_NOTICE,"strcmp length is %s\n", &extn[extnLength - apportLength]);
+    return (strcmp(&extn[extnLength - apportLength], CRASH_EXTN)) == 0;
 }
 
 static void trapCrashFile(struct inotify_event *i)
@@ -50,20 +50,20 @@ static void trapCrashFile(struct inotify_event *i)
     // syslog(LOG_DEBUG, "cmdStr is %s at event %d\n", cmdStr, i->mask);
 
     if (checkFileExtn(i->name, CRASH_EXTN)) {
-	    // syslog(LOG_DEBUG, "File %s has extension %s\n", i->name, CRASH_EXTN);
-	    if (i->mask & IN_CLOSE_WRITE) {
-		    system(cmdStr);
-		    syslog(LOG_NOTICE, "cmdStr is %s at event %d\n", cmdStr, i->mask);
-	    }
+        // syslog(LOG_DEBUG, "File %s has extension %s\n", i->name, CRASH_EXTN);
+        if (i->mask & IN_CLOSE_WRITE) {
+            system(cmdStr);
+            syslog(LOG_NOTICE, "cmdStr is %s at event %d\n", cmdStr, i->mask);
+        }
     } /* else {
-	    syslog(LOG_DEBUG, "File %s does not have extension %s\n", i->name, CRASH_EXTN);
-    } */
+         syslog(LOG_DEBUG, "File %s does not have extension %s\n", i->name, CRASH_EXTN);
+         } */
 
     /*
-    if (i->mask & IN_CREATE)        system(cmdStr);
-    if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
-    if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
-    */
+       if (i->mask & IN_CREATE)        system(cmdStr);
+       if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+       if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
+       */
 
 }
 
@@ -86,7 +86,7 @@ int main()
 
     inotifyFd = inotify_init();                 /* Create inotify instance */
     if (inotifyFd == -1)
-	syslog(LOG_NOTICE, "inotify_init");
+        syslog(LOG_NOTICE, "inotify_init");
 
     // wd = inotify_add_watch(inotifyFd, CRASH_PATH, IN_CREATE|IN_ATTRIB|IN_MODIFY); /* We only care when a crash file gets created */
     // wd = inotify_add_watch(inotifyFd, CRASH_PATH, IN_CLOSE_WRITE); /* We only care when a crash file gets created */
@@ -109,7 +109,7 @@ int main()
         for (p = buf; p < buf + numRead; ) {
             event = (struct inotify_event *) p;
             trapCrashFile(event);
-	    syslog(LOG_NOTICE, "Got invoked\n");
+            syslog(LOG_NOTICE, "Got invoked\n");
 
             p += sizeof(struct inotify_event) + event->len + 1;
         }
