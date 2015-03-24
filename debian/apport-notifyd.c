0c8a9bb133acaf80cb0c00fea973f2de5339a0d0
Yuru Shao <shaoyuru@gmail.com>
2015-03-23 22:05:28 -0400

    only 1 instance per user

--- debian/apport-notifyd.c
+++ debian/apport-notifyd.c
@@ -15,7 +15,11 @@
 #include <limits.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 #define PROG "apport-bug "
@@ -67,6 +71,60 @@ static void trapCrashFile(struct inotify_event *i)
 
 }
 
+int isNotifydRunning() 
+{
+    DIR *d;
+
+    if ( (d = opendir("/proc")) == NULL ) {
+        // should exit or continue?
+        exit(-1);
+    }
+
+    struct dirent *dir;
+    struct stat st;
+
+    int cmdFileFd;
+    char cmdFile[64];
+    char procInfoDir[64];
+    char buf[512];
+
+    while ( (dir = readdir(d)) != NULL ) {
+        // we ignore items that are not directory and current process
+        if ( dir->d_type != DT_DIR || strtol(dir->d_name, NULL, 10) == getpid() ) 
+            continue;   
+
+        // clear strings
+        memset(procInfoDir, '\0', sizeof(procInfoDir));
+        memset(cmdFile, '\0', sizeof(cmdFile));
+        memset(buf, '\0', sizeof(buf));
+ 
+        snprintf(procInfoDir, sizeof(procInfoDir), "/proc/%s", dir->d_name);
+        stat(procInfoDir, &st);
+
+        // check if apport-notifyd has been started by the same user
+        if ( st.st_uid == getuid() ) { 
+            snprintf(cmdFile, sizeof(cmdFile), "%s/cmdline", procInfoDir);                
+
+            if ( (cmdFileFd = open(cmdFile, O_RDONLY)) > 0 ) {
+                read(cmdFileFd, buf, sizeof(buf)); // should check ret value
+
+                if ( strstr(buf, "apport-notifyd") != NULL ) {
+                    close(cmdFileFd);
+                    closedir(d);
+                    return 1;
+                }
+
+                close(cmdFileFd);
+            }
+        }
+            
+    }
+
+    closedir(d);
+    
+    return 0;
+}
+
 
 int main()
 {
@@ -76,10 +134,15 @@ int main()
     char *p;
     struct inotify_event *event;
 
+
+    if ( isNotifydRunning() ) {
+        exit(-1);                
+    }
+
     daemon(0,0);
 
     /* Open a connection to the syslog server */
-    openlog("apport-notifyd",LOG_NOWAIT|LOG_PID,LOG_USER); 
+    openlog("apport-notifyd", LOG_NOWAIT|LOG_PID,LOG_USER); 
 
     /* Sends a message to the syslog daemon */
     syslog(LOG_NOTICE, "Successfully started daemon\n"); 
