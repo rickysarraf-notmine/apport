0195df3044ee3180fd9b98ce7f24a614b4dc6a80
Yuru Shao <shaoyuru@gmail.com>
2015-06-20 23:26:20 -0400

    add bug report details window

--- gtk/display_bugreports.py
+++ gtk/display_bugreports.py
@@ -11,16 +11,42 @@ class BugReportListBoxRow(Gtk.ListBoxRow):
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         self.add(hbox)
 
-        textBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         header = Gtk.Label("Bug #: %s" % self.report.bug_num, xalign=0)
-        # TODO: highlight header
+        header.set_markup("<b>Bug #: %s</b>" % self.report.bug_num)
         desc = Gtk.Label(self.report.subject, xalign=0)
+        desc.set_line_wrap(True)
+
+        textBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         textBox.pack_start(header, True, True, 0)
         textBox.pack_start(desc, True, True, 0)
 
-        btnViewBug = Gtk.Button('View')
+        # set listener
+        self.btnViewBug = Gtk.Button('View')
+        self.btnViewBug.connect("clicked", self.callbacks)
+
         hbox.pack_start(textBox, True, True, 0)
-        hbox.pack_start(btnViewBug, True, True, 0)
+        hbox.pack_start(self.btnViewBug, False, False, 0)
+
+    def callbacks(self, obj):
+        if obj == self.btnViewBug:
+            print("To view bug # %s" % self.report.bug_num)
+            bugWin = BugReportDetails(self.report)
+            bugWin.show_all()
+        
+
+class BugReportDetails(Gtk.Window):
+    '''
+    Details of a single bug report
+    '''
+
+    def __init__(self, report):
+        Gtk.Window.__init__(self, title=("%s: bug # %s" % (report.package, report.bug_num)))
+        self.report = report
+        self.set_size_request(500, 400)
+        logs = debianbts.get_bug_log(self.report.bug_num)
+        for l in logs:
+            print("msg # %s" % l['msg_num'])
+            print(l['body'])
 
 
 class BugReportListWindow(Gtk.Window):
@@ -46,8 +72,9 @@ class BugReportListWindow(Gtk.Window):
         for r in reportList:
             listbox.add(BugReportListBoxRow(r))
 
+
 if __name__ == '__main__':
-    win = BugReportListWindow('python-debianbts')
+    win = BugReportListWindow('apport-gtk')
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
     Gtk.main()
